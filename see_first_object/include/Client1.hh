ec88ac51fe9cf4083eaa7fce0202183dce4646ab
Andrej Pangercic <andrejpan@oktaeder.org>
2015-08-31 13:14:56 +0200

    adding second project

---
+++ see_first_object/include/Client1.hh
@@ -0,0 +1,101 @@
+#ifndef CLIENT1_H
+#define CLIENT1_H
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/rendering/rendering.hh>
+#include "gazebo/rendering/Visual.hh"
+#include "gazebo/rendering/Scene.hh"
+#include "gazebo/rendering/DynamicLines.hh"
+#include "gazebo/rendering/RenderTypes.hh"
+
+#include "gazebo/physics/physics.hh"
+#include "gazebo/transport/TransportTypes.hh"
+#include "gazebo/msgs/MessageTypes.hh"
+
+#include "gazebo/common/Time.hh"
+#include "gazebo/common/Plugin.hh"
+#include "gazebo/common/Events.hh"
+
+#include <gazebo/gui/GuiIface.hh>
+
+#include <string.h>
+#include <stdlib.h>     /* strtol */
+
+#define BufferLength 25
+#define SERVER "192.168.101.159"
+#define SERVPORT 1234
+
+namespace gazebo
+{
+/// \Class Client1 Client1.hh
+    class Client1 : public VisualPlugin
+    {
+        /// \brief Socket descriptor for client.
+        private: int sd;
+
+        /// \brief Number of received bytes from server.
+        private: int rc;
+
+        /// \brief Received bytes from server.
+        private: char buffer[];
+
+        /// \brief Data for server.
+        private: struct sockaddr_in serveraddr;
+
+        /// \brief Server ip address.
+        private: struct hostent *he;
+
+        /// \brief Thread recive data from server.
+        private: boost::thread *listeningThread;
+
+        /// \brief Camera pointer.
+        private: rendering::UserCameraPtr mUserCam;
+
+        /// \brief Pointer to the function OnUpdate().
+        private: event::ConnectionPtr updateFunction;
+
+        /// \brief Standard Ogre scene node.
+        private: Ogre::SceneNode *sceneNode;
+
+        /// \brief Standard Ogre entity.
+        private: Ogre::Entity *entity;
+
+        /// \brief Srandard Ogre scene manager.
+        private: Ogre::SceneManager *manager;
+
+        /// \brief Float values for (x,y) point on screen.
+        private: float x,y;
+
+        /// \brief Int values for (x,y) point on screen.
+        private: int xI, yI;
+
+        /// \brief Gives information about received string.
+        private: char * pch;
+
+        /// \brief Pointer to the next number in string.
+        private: char * pEnd;
+
+        /// \brief
+        private: rendering::VisualPtr VPtr;
+
+        /// \brief Minimal fixation data.
+        private: math::Vector2i coords2i;
+
+        /// \brief
+        private: math::Vector3 contactPoint;
+
+        /// \brief Load.
+        public: void Load(rendering::VisualPtr _parent, sdf::ElementPtr /*_sdf*/);
+
+        /// \brief OnUpdate.
+        public: void OnUpdate();
+
+        /// \brief Connection to the server.
+        private: void ConnectToServer();
+
+        /// \brief Getting data from server in thread.
+        private: void ListenThread();
+
+    };
+}
+#endif // CLIENT1_H
---
+++ see_first_object/src/Client1.cc
@@ -0,0 +1,121 @@
+#include <Client1.hh>
+
+using namespace std;
+using namespace gazebo;
+
+//////////////////////////////////////////////////
+void Client1::Load(rendering::VisualPtr _parent, sdf::ElementPtr /*_sdf*/)
+{
+    cout << "Load function has been called" << endl;
+    this->ConnectToServer();
+
+    this->listeningThread = new boost::thread(boost::bind(&Client1::ListenThread, this));
+
+    // Listen to the update event. This event is broadcast every
+    // simulation iteration.
+    this->updateFunction = event::Events::ConnectRender(
+        boost::bind(&Client1::OnUpdate, this));
+
+
+    this->manager = gui::get_active_camera()->GetScene()->GetManager();
+    this->sceneNode = this->manager->getRootSceneNode()->createChildSceneNode("MySphere");
+    //this->sceneNode = this->manager->getRootSceneNode()->createChildSceneNode();
+    this->entity = this->manager->createEntity("MySphere", Ogre::SceneManager::PT_SPHERE);
+    this->entity->setMaterialName("Gazebo/Red");
+    this->sceneNode->setVisible(true);
+    this->entity->setVisible(true);
+    this->sceneNode->setScale(0.001, 0.001, 0.001);
+    this->sceneNode->setPosition(0, 0, 0);
+    this->sceneNode->attachObject(entity);
+    this->xI = 0;
+    this->yI = 0;
+
+}
+
+//////////////////////////////////////////////////
+void Client1::OnUpdate()
+{
+    //cout << "SystemSubscriber: Entering OnUpdate() " << endl;
+    this->mUserCam = gui::get_active_camera();
+    this->coords2i.x = this->xI;
+    this->coords2i.y = this->yI;
+    //printf("%d, %d\n", this->coords2i.x, this->coords2i.y); //this is working
+    if (this->mUserCam->GetScene()->GetFirstContact(this->mUserCam, this->coords2i, this->contactPoint))
+    {
+        this->sceneNode->setPosition(this->contactPoint.x, this->contactPoint.y, this->contactPoint.z);
+        //printf("%f, %f, %f\n", , this->contactPoint.y, this->contactPoint.z);
+    }
+}
+
+//////////////////////////////////////////////////
+void Client1::ConnectToServer()
+{
+
+    if( (this->he=gethostbyname(SERVER)) == NULL )
+    {
+        perror("gethostbyname()");
+        exit(1);
+    }
+    else
+        printf("Client-The remote host is: %s\n", SERVER);
+
+    if ((this->sd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Client-socket() error");
+        exit(-1);
+    }
+    else
+        printf("Client-socket() OK\n");
+
+    this->serveraddr.sin_family = AF_INET;
+    this->serveraddr.sin_port = htons(SERVPORT);
+    this->serveraddr.sin_addr = *((struct in_addr *) this->he->h_addr);
+
+    // zero the rest of the struct
+    memset(&(this->serveraddr.sin_zero), '\0', 8);
+
+
+    if(connect(this->sd, (struct sockaddr *)&this->serveraddr, sizeof(this->serveraddr)) == -1)
+    {
+        perror("connect()");
+        close(this->sd);
+        exit(1);
+    }
+    else
+    {
+        printf("Client-The connect() is OK...\n");
+    }
+}
+
+//////////////////////////////////////////////////
+void Client1::ListenThread()
+{
+    boost::posix_time::millisec workTime(1);
+    while (1) {
+        memset(buffer, 0, BufferLength);
+        this->rc = read(this->sd, &buffer[0], BufferLength);
+        if (this->rc < 0)
+        {
+            perror("Client-read() error");
+            close(sd);
+            exit(-1);
+        } else if (this->rc == 0)
+        {
+            printf("Server program has issued a close()\n");
+        }
+        else
+        {
+            // lets be sure, that we recieve right data
+            pch=strchr(this->buffer,' ');
+            if(pch!=NULL)
+            {
+                xI = strtol(this->buffer, &pEnd, 10);
+                yI = strtol(pEnd, NULL, 10);
+                //printf("%d, %d\n", xI, yI);
+            }
+        }
+        boost::this_thread::sleep(workTime);
+    }
+}
+
+GZ_REGISTER_VISUAL_PLUGIN(Client1)
