bf3400937ed1c4b284c02812713725cd8d7556ee
Andrej Pangercic <andrejpan@oktaeder.org>
2015-09-01 15:15:04 +0200

    mongodb update1

---
+++ gazebo_mongodb_sending/worlds/build/src/CMakeFiles/Client1.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrejpan/Documents/gazebo/gazebo_mongodb_sending/src/Client1.cc" "/home/andrejpan/Documents/gazebo/gazebo_mongodb_sending/build/src/CMakeFiles/Client1.dir/Client1.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/gazebo-6.1"
+  "/usr/include/sdformat-3.2"
+  "/usr/include/ignition/math2"
+  "/usr/include/OGRE"
+  "/usr/include/OGRE/Terrain"
+  "/usr/include/OGRE/Paging"
+  "../include"
+  "/home/andrejpan/mongo-client-install/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
